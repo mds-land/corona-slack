d18e6e6a38cf9ab90b8cc5f9a3a96fbfd08c94ea
Daniil Mikhailov <info@mdsina.ru>
2020-04-23 15:03:04 +0700

    Add delayed send to slack channels

--- src/main/java/com/github/mdsina/corona/corona/web/CoronaCommandsController.java
+++ src/main/java/com/github/mdsina/corona/corona/web/CoronaCommandsController.java
@@ -1,12 +1,12 @@
 package com.github.mdsina.corona.corona.web;
 
 import com.github.mdsina.corona.corona.CoronaSlackDataService;
+import com.github.mdsina.corona.slack.SlackMessageSender;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
-import io.reactivex.Single;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,21 +19,30 @@ import lombok.extern.slf4j.Slf4j;
 public class CoronaCommandsController {
 
     private final CoronaSlackDataService coronaSlackDataService;
+    private final SlackMessageSender slackMessageSender;
 
     @Post(value = "/stats", consumes = {MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
-    public Single<Map<String, Object>> stats(@Body Map body) {
+    public String stats(@Body Map body) {
         log.debug(body.toString());
 
-        return coronaSlackDataService
-            .getActualStatsResponse(getCountriesFromBody(body), body.get("channel_id"));
+        coronaSlackDataService
+            .getActualStatsBlocks(getCountriesFromBody(body))
+            .map(blocks -> slackMessageSender.sendMessage((String) body.get("channel_id"), blocks))
+            .subscribe();
+
+        return "Ok, stats will be send shortly.";
     }
 
     @Post(value = "/image", consumes = {MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
-    public Single<Map<String, Object>> graph(@Body Map body) {
+    public String graph(@Body Map body) {
         log.debug(body.toString());
 
-        return coronaSlackDataService
-            .getHistoricalStatsResponse(getCountriesFromBody(body), body.get("channel_id"));
+        coronaSlackDataService
+            .getHistoricalStatsBlocks(getCountriesFromBody(body))
+            .map(blocks -> slackMessageSender.sendMessage((String) body.get("channel_id"), blocks))
+            .subscribe();
+
+        return "Ok, chart will be send shortly.";
     }
 
     private List<String> getCountriesFromBody(Map body) {
