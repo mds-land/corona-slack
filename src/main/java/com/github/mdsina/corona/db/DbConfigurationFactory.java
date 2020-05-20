0e7d4b7d0e5ec8038ba8417bcab910ed3e305c23
Daniil Mikhailov <info@mdsina.ru>
2020-05-21 03:30:20 +0700

    Fix db dialect selecting

--- src/main/java/com/github/mdsina/corona/db/DbConfigurationFactory.java
+++ src/main/java/com/github/mdsina/corona/db/DbConfigurationFactory.java
@@ -2,9 +2,11 @@ package com.github.mdsina.corona.db;
 
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Value;
 import javax.sql.DataSource;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultConfiguration;
 import org.jooq.impl.DefaultDSLContext;
 
@@ -17,9 +19,13 @@ public class DbConfigurationFactory {
     }
 
     @Bean
-    public Configuration configuration(DataSource dataSource) {
+    public Configuration configuration(
+        DataSource dataSource,
+        @Value("${jooq.datasources.default.sql-dialect}") String dialect
+    ) {
         DefaultConfiguration configuration = new DefaultConfiguration();
         configuration.setDataSource(dataSource);
+        configuration.setSQLDialect(SQLDialect.valueOf(dialect));
         return configuration;
     }
 }
