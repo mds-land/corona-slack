2c1a58d3ecebe02b865b752532f8099ddb1112c7
Daniil Mikhailov <info@mdsina.ru>
2020-12-07 16:55:36 +0700

    fix retry

--- src/main/java/com/github/mdsina/corona/scheduling/RetryableTaskRunner.java
+++ src/main/java/com/github/mdsina/corona/scheduling/RetryableTaskRunner.java
@@ -16,16 +16,20 @@ public class RetryableTaskRunner {
     @Named(TaskExecutors.SCHEDULED)
     private final TaskScheduler taskScheduler;
 
-    public void run(Runnable runnable, Duration delay) {
+    public void run(Runnable runnable, Duration delay, int count) {
+        if (count <= 0) {
+            log.info("Retry count == 0. Stopping.");
+            return;
+        }
         try {
             runnable.run();
         } catch (Throwable e) {
             log.error("Error occurred on running task. Task will be rescheduled.", e);
-            taskScheduler.schedule(delay, () -> run(runnable, delay));
+            taskScheduler.schedule(delay, () -> run(runnable, delay, count - 1));
         }
     }
 
     public void run(Runnable runnable) {
-        run(runnable, Duration.ofSeconds(60L));
+        run(runnable, Duration.ofSeconds(60L), 2);
     }
 }
